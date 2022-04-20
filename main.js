6ed090491a09bca35069e288bf8d1da505768e9a
unknown <detroyexna.com>
2022-04-21 00:26:38 +0700

    commit

--- main.js
+++ main.js
@@ -1,44 +1,60 @@
-Vue.createApp({
-    data() {
-      return {
-        message: "Hello Vue!",
-        header: {
-          title: "Devi triano",
-          description:
-            "Ini adalah tempat saya untuk berbagi segala pengetahuan yang saya peroleh tentang koding dan teknologi.",
-          social: {
-            ig: {
-              url: "https://instagram.com/detroyfx",
-              title: "devi triana",
-            },
-            TikTok: {
-                "url": "https://www.tiktok.com/@menyonnn__?lang=id-ID",
-                "title": "devitriana"
-            }
-            yt: {
-              url: "https://www.youtube.com/channel/UCnvQ-f17cZduon6idsEaDOg",
-              title: "devi triana",
-            },
-          },
-          imageProfile:
-             "https://raw.githubusercontent.com/Devitrianasuci/tekweb2022/master/images/dep.png",
-        },  
-        articles: [
-          {
-            title: 'The Magic of Vue',
-            description: "This is your first looping using Vue",
-            thumbnail: 'https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80'
-          }
-        ]
-      };
-    },
-    mounted:()=>{    
-      axios.get("https://raw.githubusercontent.com/Devitrianasuci/tekweb2022/master/content/header.json")
-      .then((res)=>{
-        console.log(res.data);
-      })
-      .catch((error)=>{
-        console.log(error);
-      })
+const data = {
+  data () {
+    return {
+      content : {
+        nama : "Devi Triana Suci",
+        deskripsi : "Sebuah perjalanan ilmu menjelajah kode. Disini saya berbagi tentang perjalanan saya menjelajah berbagai bahasa kode komputer."
+      },
+      social_media : {
+        instagram : {
+          id : 0,
+          username : "devi triana",
+          link : "https://instagram.com/detroyfx"
+        },
+        Tiktok : {
+          id : 1,
+          username : "menyon",
+          link : "https://www.tiktok.com/@menyonnn__?lang=id-ID"
+        },
+        youtube : {
+          id : 2,
+          username : "devitriana",
+          link : "https://www.youtube.com/channel/UCnvQ-f17cZduon6idsEaDOg"
+        }
+      },
+      article : [
+        {
+          id : 1,
+          image : "./images/html11.jpg",
+          heading : "HyperText Markup Language (HTML)",
+          deskripsi : "intermediate"
+        },
+        {
+          id : 2,
+          image : "./images/css1.jpg",
+          heading : "Cascading Style Sheets",
+          deskripsi : "intermediate"
+        },
+        {
+          id : 3,
+          image : "./images/js1.jpg",
+          heading : "Java Script (JS)",
+          deskripsi : "intermediate"
+        }
+      ],
+      skill : [
+        {
+          image : "./images/python1.jpg",
+          nama : "Java Script",
+          level : "intermediate"
+        },
+        {
+          image : "./images/sql.jpg",
+          nama : "HTML",
+          level : "intermediate"
+        }
+      ]
     }
-  }).mount("#app");
+  }
+}
+Vue.createApp(data).mount('#app');
